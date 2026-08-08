bdd210e9a87e724b62269beabebfeab9f5c1c6de
jernyx <134298322+jernyx@users.noreply.github.com>
2026-08-08 12:32:15 +0200

    Update YouTube Filters.js

--- Browser/Tampermonkey/YouTube Filters.js
+++ Browser/Tampermonkey/YouTube Filters.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Filters
 // @namespace    http://tampermonkey.net/
-// @version      5.3
+// @version      5.4
 // @description  Combines subscription filters and metadata duration injection into a single script.
 // @author       You
 // @match        *://*.youtube.com/*
